3a18f73437e3274013db2abf6d3285c55d2502c0
Pavel Šturc <psturc@redhat.com>
2018-02-23 12:10:39 +0100

    Modified server port and address

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var mockServerClient = require('mockserver-client').mockServerClient;
-var mockserver = require('mockserver-node');
+const mockServerClient = require('mockserver-client').mockServerClient;
+const mockserver = require('mockserver-node');
+
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 1080
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0"
 
 mockserver.start_mockserver({
-                serverPort: 1080,
+                serverPort: server_port,
                 proxyPort: 1090,
                 verbose: true
 }).then(function() {
-  mockServerClient("localhost", 1080).mockAnyResponse({
+  mockServerClient(server_ip_address, server_port).mockAnyResponse({
     "httpRequest": {
         "path": "/metrics"
     },
